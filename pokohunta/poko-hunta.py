464f960571e07788d601fc02c9fe15138ecc5cfd
davicii-ii <aviciishawn@gmail.com>
2020-04-23 20:33:07 +0300

    trying to get poko-hunta.py to work.

--- pokohunta/poko-hunta.py
+++ pokohunta/poko-hunta.py
@@ -1,10 +1,9 @@
 from pyrogram import Client, Audio
 import sys, pickle, os
 
-jugged = -1001124792940
-decrate =  -1001280481543
-de_dict = {}
-de_list = []
+decrate = -1001280481543
+api_id = 314504
+api_hash = '8c64c308e6f0186d495ae1e92a1c228d'
 
 def save_obj(obj, name):
     try:
@@ -16,43 +15,24 @@ def save_obj(obj, name):
 def load_obj(name):
     with open('obj/' + name + '.pkl', 'rb') as f:
         return pickle.load(f)
-
-def history_indict():
-    try:
-        for message in app.iter_history(decrate):
-            if not message.audio:
-                pass
-            else:
-                de_dict.update({message.audio.title : message.audio.duration})
-                save_obj(de_dict, "de_history")
-    except(AttributeError) as e:
-        print(e)
-
-def get_longballs():
-    for n, t in de_dict.items():
-        if t >= 600:
-            de_list.append(n)
                     
-
-with Client("de_crate", config_file="de_config.ini") as app:
-    de_dict.update(load_obj("de_history"))
-    if len(de_dict) != 0:
-        print("\nnot empty\n")
-        get_longballs()
-        for i in de_list:
-            print(i)
-        print("\nFound", len(de_list), "items.")
-    else:
-        print("empty")
-        history_indict()
-        
+with Client('de_crate', api_id, api_hash) as app:
+    print('glitch')
+    try:
+        with open('pokohunta/obj/de_his.txt', 'w') as f:
+            try:
+                for msg in app.iter_history(decrate):
+                    if msg.media:
+                        print(msg, file=f)
+            except(AttributeError) as e:
+                print(e)
+    except(FileNotFoundError) as e:
+        os.system('touch pokohunta/obj/de_hist.txt')
+        print('glitch2')
 
 """
 TODO:
-    we want to delete clones.
-    we check for clones by time & file name & compare them to decrate-history.txt
-    constantly check for updates
-    find the name of the file by checking length of song
     *add t.me links to file 
     *update telegra.ph
-    """
+
+"""
