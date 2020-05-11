405da62fdf37b4c924906bde650564eb5df221a6
davicii-ii <aviciishawn@gmail.com>
2020-05-12 01:24:31 +0300

    clean qs.py & pokohunta/main.py :) 	:) :)

--- pokohunta/main.py
+++ pokohunta/main.py
@@ -1,11 +1,2 @@
-from pyrogram import Client, Audio
-import os
+from qs import bot_app, man_app
 
-decrate = -1001280481543
-api_id = 314504
-api_hash = '8c64c308e6f0186d495ae1e92a1c228d'
-
-def de_history():
-    with open('pokohunta/obj/de_record.txt', 'w') as f:
-        for msg in app.iter_history(decrate):
-            print(msg, file=f)
--- pokohunta/qs.py
+++ pokohunta/qs.py
@@ -2,13 +2,9 @@ from pyrogram import Client
 
 api_id = 314504
 api_hash = '8c64c308e6f0186d495ae1e92a1c228d'
-Client.terminate()
-print('tetminated!')
+phone_number = '+254714388863'
+bot_token = '592747688:AAEvH6J90ZyuxvxATwVamdv8PculFLkI4Pk'
 
-def bot_hunta():
-    with Client('bot_hunta', api_id, api_hash) as app:
-        app.send_message('wateruwant', 'botbou2hunt')
+bot_app = Client('bot_hunta', api_id, api_hash)
+man_app = Client('man_hunta', api_id, api_hash)
 
-def man_hunta():
-    with Client('man_hunta', api_id, api_hash) as app:
-        app.send_message('me', 'manbou2hunt')
